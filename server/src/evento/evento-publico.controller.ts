e9473ff8e4bfa1d537ce3f196eadaea198a55db7
Galdcvn <caio.vn.gald@gmail.com>
2026-08-18 21:56:45 -0300

    fix(server): add @Public() to eventos publicos controller
    Rotas GET /api/eventos/publicos estavam retornando 403 porque o
    JwtAuthGuard global bloqueava sem o decorator @Public()

--- server/src/evento/evento-publico.controller.ts
+++ server/src/evento/evento-publico.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { Public } from '../common/decorators/public.decorator';
 import { BuscarEventosDto } from './dto/buscar-eventos-publicos.dto';
 import { EventoService } from './evento.service';
 
@@ -6,11 +7,13 @@ import { EventoService } from './evento.service';
 export class EventoPublicoController {
   constructor(private readonly eventoService: EventoService) {}
 
+  @Public()
   @Get()
   listar(@Query() filtros: BuscarEventosDto) {
     return this.eventoService.listarPublicos(filtros);
   }
 
+  @Public()
   @Get(':id')
   detalhe(@Param('id', ParseIntPipe) id: number) {
     return this.eventoService.detalhePublico(id);
